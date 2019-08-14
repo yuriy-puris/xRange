2b26b562609e18807e584957de436a4b1a5b1a88
Yuriy Puris <donovanyuriy@gmail.com>
2019-08-14 09:37:09 +0300

    first

--- server/products/models.py
+++ server/products/models.py
@@ -1,7 +1,26 @@
-from django.db import models
+from djongo import models
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_shop_id = models.IntegerField()
     product_title = models.TextField()
     product_description = models.TextField(blank=True)
+
+class MyShopTags(models.Model):
+    tag_key = models.CharField(max_length=100)
+    tag_valuee = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.tag_key
+
+class MyShops(models.Model):
+    shop_id = models.AutoField(primary_key=True)
+    shop_url = models.TextField()
+    shop_card_tags = models.ArrayModelField(
+        model_container = MyShopTags,
+    )
+
+    
